47169059d8ecf1497576ded2c3d8facfb1357f2b
Lukesudom <lukemarshall511@gmail.com>
2022-07-27 21:13:20 +1000

    added prompts for add employee

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ import Intern from './lib/Intern';
 import fs from 'fs';
 
 import inquirer from 'inquirer';
+import { report } from 'process';
+import { triggerAsyncId } from 'async_hooks';
 
 const teamArray = [];
 
@@ -22,7 +24,7 @@ const addManager = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'Who is the team manager?'
+            message: 'Who is the team manager?',
 
             validate: nameInput => {
                 if (nameInput) {
@@ -38,14 +40,14 @@ const addManager = () => {
         {
             type: 'input',
             name: 'ID',
-            message: 'What is the managers ID?'
+            message: 'What is the managers ID?',
 
             validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
+                if (isNaN(nameInput)) {
                     console.log ("Please enter a valid manager ID!");
                     return false;
+                } else {
+                    return true;
                 }
             }
 
@@ -54,10 +56,11 @@ const addManager = () => {
         {
             type: 'input',
             name: 'email',
-            message: 'What is the email address of the manager?'
+            message: 'What is the email address of the manager?',
 
-            validate: nameInput => {
-                if (nameInput) {
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
                     return true;
                 } else {
                     console.log ("Please enter a valid email address!");
@@ -70,17 +73,105 @@ const addManager = () => {
         {
             type: 'input',
             name: 'officeNumber',
-            message: 'Please enter the managers office number'
+            message: 'Please enter the managers office number',
 
             validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
+                if (isNaN(nameInput)) {
                     console.log ("Please enter a valid office number!");
                     return false;
+                } else {
+                    return true;
                 }
             }
 
         },
     ])
-}
+
+    .then (managerInput => {
+        const { name, id, email, officeNumber } = managerInput;
+        const manager = new Manager (name, id, email, officeNumber);
+        teamArray.push(manager);
+        console.log(manager);
+    })
+};
+
+//Employee and intern prompts
+
+const addEmployee = () => {
+    console.log ('Adding Employees to the system')
+};
+
+
+return inquirer.prompt ([
+    {
+        name: 'role',
+        type: 'list',
+        message: "Choose your employee role",
+        choices: ['Engineer','Intern']
+    },
+    {
+        name: 'name',
+        type: 'input',
+        message: 'What is the employees name?',
+         validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else
+            console.log ("please enter an employee name");
+            return false;
+        }
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: "Please enter an employee email",
+        validate: email => {
+            valid =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+            if (valid) {
+                return true;
+            } else {
+                console.log ("Enter a valid email address!")
+                return false;
+            }
+        }
+    },
+
+    {
+        name: 'school',
+        type: 'input',
+        message: "Enter the employees GitHub username",
+        when: (input) => input.role === 'Engineer',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log ("Please enter a valid GitHub username");
+                return false;
+            }
+        }
+    },
+
+    {
+        name: 'school',
+        type: 'input',
+        message: 'Enter the interns University',
+        when: (input) => input.role === 'Intern',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log ("Please enter a valid University!");
+                return false;
+            }
+        }
+    },
+
+    {
+        name: 'confirmAddEmployee',
+        type: 'confirm',
+        message: 'Are you sure you would like to add this employee?',
+        default: false,
+    }
+])
+
+.then ()
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // server.js
 
-const express = require('express');
-const app = express();
-const expressValidator = require('express-validator');
-const PORT = 3000;
-app.listen(PORT, function(req, res){
-   console.log('Server is running on PORT: ',PORT);
-});
+// const express = require('express');
+// const app = express();
+// const expressValidator = require('express-validator');
+// const PORT = 3000;
+// app.listen(PORT, function(req, res){
+//    console.log('Server is running on PORT: ',PORT);
+// });
